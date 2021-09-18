f91dcd9e07a1ca6610c225ddafa4dc20c7b371e4
TheGetsuzoThunder <znxthunder@gmail.com>
2021-09-18 17:56:44 +0800

    Up file

---
+++ database/latx/latx.js
@@ -0,0 +1,30 @@
+exports.menu = (pushname, sender, prefix, banChats, ucapanWaktu, timeWib, timeWit, timeWita) => {
+return`
+ ${ucapanWaktu} 𝐎𝐧𝐢𝐢 𝐂𝐡𝐚𝐧
+ 名前 	: ${pushname}
+ タグ     : @${sender.split('@')[0]}
+
+𝐁𝐎𝐓 𝐁𝐘 𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐗7
+
+ 時間 🕑 
+
+ 𝐖𝐢𝐛    : ${timeWib}
+ 𝐖𝐢𝐭    : ${timeWit}
+ 𝐖𝐢𝐭𝐚   : ${timeWita}
+
+✦  𝐁𝐲: 𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐗7
+
+  🔔  𐎟 𝐀𝐛𝐨𝐮𝐭
+
+✦ 𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐗7 𝐘𝐓
+https://youtube.com/c/ThunderX7%E3%81%B907
+
+✦ 𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐗7 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦
+https://instagram.com/thunderx.7
+
+ 𝐏𝐫𝐞𝐟𝐢𝐱 ❕: 「 𝑀𝑈𝐿𝑇𝐼 」
+ 𝐒𝐭𝐚𝐭𝐮𝐬 🌟: ${banChats ? "𝐒𝐞𝐥𝐟 𝐌𝐨𝐝𝐞" : "𝐏𝐮𝐛𝐥𝐢𝐜 𝐌𝐨𝐝𝐞"}
+
+
+`
+}

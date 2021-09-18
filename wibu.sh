388fdf229df62bfb4e6a49abbf57ef05ad620616
Thunder X7 <86151730+TheGetsuzoThunder@users.noreply.github.com>
2021-09-18 18:23:53 +0800

    Installer
    Installer

---
+++ wibu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#update tambahan
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y nodejs libwebp ffmpeg wget tesseract ffmpeg npm nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+echo "Okeh udah selesai sekarang lu tinggal path nya aja langsung npm start"
+sleep 6s
+npm start
